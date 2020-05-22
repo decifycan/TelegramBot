cb50eee5bc9cddcee9a9340b2d505e542524574c
Ishan Pandey <42055735+ishhaaannnn@users.noreply.github.com>
2020-05-22 10:49:00 +0530

    Update stocks_price.py

--- stocks_price.py
+++ stocks_price.py
@@ -1,7 +1,5 @@
-import pandas as pd
 from bs4 import BeautifulSoup
 import requests
-from datetime import date,time
 
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'}
 
